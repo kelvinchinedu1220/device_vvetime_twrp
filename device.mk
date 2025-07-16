10bd760c0cf46a3f2b17593b4f6601cf6cc5301a
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-16 23:27:03 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,12 @@
 #
 
 LOCAL_PATH := device/vvetime/aeon6755_66_m
+
+# Usb
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=adb \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0 \
+    service.adb.root=1 \
+    ro.debuggable=1
